2a3ecbf28579732324e587f619966b53ca94246a
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-12 16:46:46 +1100

    binding a declared function checkCode to click event instead of an anonymous function

--- scripts/chapter3_my_scripts.js
+++ scripts/chapter3_my_scripts.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
     $(".guess_box").click(checkForCode);
+    hideCode();
 });
 
 function checkForCode(){
     /* Act on the event */
     //alert("you clicked me!");
-    var discount = Math.floor((Math.random()*5)+5);
-    var discount_msg = "<p>Your discount is: " + discount + "%</p>";
-    $(".guess_box p").remove();
+    //
+    var discount_msg;
+    if($.contains(this, document.getElementById("has_discount"))){
+        var discount = getRandom(5);
+        discount_msg = "<p>Your discount is: " + discount + "%</p>";
+    }else{
+        discount_msg = "<p>Sorry, no discount this time!</p>"; 
+    }
+    
+    //$(".guess_box p").remove();
     $(this).append(discount_msg);
     //$(".guess_box").unbind('click');
     $(".guess_box").each(function(){
@@ -15,3 +23,15 @@ function checkForCode(){
     });
     
 }
+function getRandom(num){
+    return Math.floor(Math.random()*num);
+}
+var hideCode = function(){
+    var numRand = getRandom(4);
+    $(".guess_box").each(function(index, value){
+        if(numRand == index){
+            $(this).append("<span id='has_discount'></span>");
+            return true;
+        }
+    });
+};
