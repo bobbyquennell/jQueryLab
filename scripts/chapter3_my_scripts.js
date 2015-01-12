49006f91d8bdabf78edbc6642aeee93525665bce
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-13 10:44:08 +1100

    requirement 1.the discount code should be text and a number between 1 and 100； 2.move the discount code to it’s own easy-to-ready area below the boxes. 3. show which one has discount code

--- scripts/chapter3_my_scripts.js
+++ scripts/chapter3_my_scripts.js
@@ -18,18 +18,28 @@ function checkForCode(){
     //
     var discount_msg;
     if($.contains(this, document.getElementById("has_discount"))){
-        var discount = getRandom(5);
-        discount_msg = "<p>Your discount is: " + discount + "%</p>";
+        var discount = getRandom(100);
+        discount_msg = "<p>Your Code : CODE" + discount + "</p>";
+
     }else{
         discount_msg = "<p>Sorry, no discount this time!</p>"; 
     }
+    $(".guess_box").each(function(index, el) {
+        if($.contains(this, document.getElementById("has_discount"))){
+            $(this).addClass('discount');
+        }
+        else{
+            $(this).addClass('no_discount');
+        }
+    });
     
     //$(".guess_box p").remove();
-    $(this).append(discount_msg);
+    $("#main").append(discount_msg);
     //$(".guess_box").unbind('click');
     $(".guess_box").each(function(){
         $(this).unbind('click');
     });
+
     
 }
 function getRandom(num){
