da8d0228da17bb8ad2a6ee1a69aa3ff634e4a236
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-14 09:39:33 +1100

    a. remove fish entrees     b. replace hamburgers with giant portable mushrooms     c. replace all meat and egg except hamburger with tofu     d. add a button to restore the menu to its original state.     e. add a button”go vegetarian” that automatically substitutes the vegetarian options on page

--- scripts/chapter4_go_vegetarian_menu.js
+++ scripts/chapter4_go_vegetarian_menu.js
@@ -3,13 +3,28 @@ $(document).ready(function() {
     $("#vegOn").click(function(event) {
         /* Act on the event */
         if(v==false){
-            v = true;
+            v = true;           
         }
+        $f = $(".fish").parent().parent().detach();
+        $ham = $(".hamburger").replaceWith('<li class="protobello"><em>protobello mushrooms</em></li>');
+        $(".meat").after('<li class="tofu"><em>tofu</em></li>');
+        $meat = $(".meat").detach();
     });
     $("#restoreMe").click(function(event) {
         /* Act on the event */
         if(v == true){
             v = false;
         }
+        $(".menu_entrees").children().first().before($f);
+        $(".protobello").replaceWith('<li class="hamburger">Hamburger</li>');
+        // for (var i = 0;i< $meat.length;i++) {
+        //     $(".tofu")[i].before($meat[i]);
+        // };
+        //  $(".tofu").remove();
+        $(".tofu").each(function(index, el) {
+            $(this).replaceWith($meat[index]);
+        });
+
+        
     });
 });
