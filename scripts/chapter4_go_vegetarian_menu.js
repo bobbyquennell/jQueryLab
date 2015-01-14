f5f940ff2e9f07d949e312cdf4ddabe63c4bd2bf
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-14 11:29:06 +1100

    extra requirement: If you can pull it off, we’d also like a leaf icon to show up next to the substituted vegetarian entrees.

--- scripts/chapter4_go_vegetarian_menu.js
+++ scripts/chapter4_go_vegetarian_menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var v=false;
+    //var $f,$ham,$meat;
     $("#vegOn").click(function(event) {
         /* Act on the event */
         if(v==false){
@@ -9,12 +10,16 @@ $(document).ready(function() {
         $ham = $(".hamburger").replaceWith('<li class="protobello"><em>protobello mushrooms</em></li>');
         $(".meat").after('<li class="tofu"><em>tofu</em></li>');
         $meat = $(".meat").detach();
+        $(".protobello").parent().parent().addClass('veg_leaf');
+        $(".tofu").parent().parent().addClass('veg_leaf');
     });
     $("#restoreMe").click(function(event) {
         /* Act on the event */
         if(v == true){
             v = false;
         }
+        $(".protobello").parent().parent().removeClass('veg_leaf');
+        $(".tofu").parent().parent().removeClass('veg_leaf');
         $(".menu_entrees").children().first().before($f);
         $(".protobello").replaceWith('<li class="hamburger">Hamburger</li>');
         // for (var i = 0;i< $meat.length;i++) {
@@ -24,6 +29,7 @@ $(document).ready(function() {
         $(".tofu").each(function(index, el) {
             $(this).replaceWith($meat[index]);
         });
+
 
         
     });
