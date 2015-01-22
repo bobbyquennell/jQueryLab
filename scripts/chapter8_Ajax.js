c4444f2ee71c1c372b140cc508c379bedaafba61
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-22 14:20:46 +1100

    change setInterval function with a self-referencing function to avoid the possible issue of setInterval.
     /* be careful with setInterval function
        digest from Head First jQuery p310
        setInterval will run even if the function it is
        calling isn’t finished yet.
        If you’re waiting on information from another server, or
        waiting on user interaction, setInterval could call your
        function again before you’re ready. Your functions may not always
        return in the order that you called them.
        startAJAXCalls(); */

--- scripts/chapter8_Ajax.js
+++ scripts/chapter8_Ajax.js
@@ -1,9 +1,29 @@
+var FREQ = 10;
 $(document).ready(function() {
     
-    var intervalID = setInterval(getXMLRacers, 5000); 
+    //var intervalID = setInterval(getXMLRacers, 5000); 
+    /* be careful with setInterval function
+    digest from Head First jQuery p310
+    setInterval will run even if the function it is
+    calling isn’t finished yet.
+    If you’re waiting on information from another server, or
+    waiting on user interaction, setInterval could call your
+    function again before you’re ready. Your functions may not always
+    return in the order that you called them.
+    startAJAXCalls(); */
 
 });
-
+function startAJAXCalls(){
+    $.ajax({
+        url:'finishers.xml',
+        cache:false,
+        dataType:'xml',
+        success:function(xml){
+             getXMLRacers(xml);
+             setTimeout(startAJAXCalls, FREQ*1000);
+        }
+    });
+}
 function getTime(){
     var a_p = "";
     var d = new Date();
@@ -23,35 +43,28 @@ function getTime(){
 
 }
 
-function getXMLRacers(){
-    $.ajax({
-        url: 'finishers.xml',
-        type: 'GET',
-        dataType: 'xml',
-        success: function(xml){
-            $('#finishers_m').empty();
-            $('#finishers_f').empty();
-            $('#finishers_all').empty();
-            // $(xml).find('runner').each(function(index, el) {
-            //     var info = '<li>Name: ' + $(this).find('fname')
-            // });
-            var result;
-            console.log("I got xml: ");
-            var runners = $(xml).find("runner");
-            runners.each(function(index, el) {
-                var result = $(this).find('fname').text() + ' ' + $(this).find('lname').text()+ ' ' + 
-                $(this).find('gender').text() + ' ' + $(this).find('time').text();
-                console.log(result);
-                var info = "<li>"+result+"</li>";
-                $("#finishers_all").append(info);
-                if($(this).find('gender').text() == 'm'){
-                    $(info).appendTo('#finishers_m');
-                }
-                else{
-                    $(info).appendTo('#finishers_f');
-                }
-            });
-        getTime();
+function getXMLRacers(xml){
+
+    $('#finishers_m').empty();
+    $('#finishers_f').empty();
+    $('#finishers_all').empty();
+            
+    var result;
+    console.log("I got xml: ");
+    var runners = $(xml).find("runner");
+    runners.each(function(index, el) {
+        var result = $(this).find('fname').text() + ' ' + $(this).find('lname').text()+ ' ' + 
+        $(this).find('gender').text() + ' ' + $(this).find('time').text();
+        console.log(result);
+        var info = "<li>"+result+"</li>";
+        $("#finishers_all").append(info);
+        if($(this).find('gender').text() == 'm'){
+            $(info).appendTo('#finishers_m');
+        }
+        else{
+            $(info).appendTo('#finishers_f');
         }
     });
+        
+    getTime();
 }
