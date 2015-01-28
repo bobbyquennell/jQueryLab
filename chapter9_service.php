c3a443a20c9462c85f251a27e60124edbbd9bf10
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-28 12:19:13 +1100

    using SQL to read data from a database---`select` PHP mySQL library API to read and display data mysql_connect() mysql_select_db() mysql_query() mysql_fetch_array()

--- chapter9_service.php
+++ chapter9_service.php
@@ -1,5 +1,17 @@
 <?php
-    mysql_connect('127.0.0.1', 'runner_db_user', 'runner_db_password') OR die('Could not connect to database.');
-    mysql_select_db('hfjq_race_info');
-    echo "Connected!";
+    function db_connection($query){
+        mysql_connect('127.0.0.1', 'runner_db_user', 'runner_db_password') OR die('Could not connect to database.');
+        mysql_select_db('hfjq_race_info');
+        /* echo "Connected!";*/
+        return mysql_query($query);
+
+    }
+    $query = "select first_name, last_name, gender, finish_time from runners order by finish_time asc";
+    $result = db_connection($query);
+
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        print_r($row);
+        # code...
+    }
+
 ?>
