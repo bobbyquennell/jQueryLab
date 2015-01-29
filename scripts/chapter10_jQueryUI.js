2bba4f8478e9113c67296f12759cf7bac12d77b7
Bobby Quennell <bobbyquennell@gmail.com>
2015-01-29 17:21:30 +1100

    Build more engaging radio buttons for users to choose the creature type

--- scripts/chapter10_jQueryUI.js
+++ scripts/chapter10_jQueryUI.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
     $("#datepicker").datepicker({changeMonth:true, changeYear:true});
+    $("#type_select").buttonset();
+    /*$("#radio1").button();
+    $("#radio2").button();
+    $("#radio3").button();*/
 });
